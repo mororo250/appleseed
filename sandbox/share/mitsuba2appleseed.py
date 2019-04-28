661d67779d8b56b55390fe3159b992e6aacf13c4
João Marcos Mororo Costa <43460229+mororo250@users.noreply.github.com>
2019-04-28 18:22:32 -0300

    Fix indentation

--- sandbox/share/mitsuba2appleseed.py
+++ sandbox/share/mitsuba2appleseed.py
@@ -955,8 +955,8 @@ def main():
 
     project = convert(tree)
 
-    asr.ProjectFileWriter().write(project, args.output_file,
-    asr.ProjectFileWriterOptions.OmitHandlingAssetFiles)
+    asr.ProjectFileWriter().write(project, args.output_file, 
+                                  asr.ProjectFileWriterOptions.OmitHandlingAssetFiles)
 
 if __name__ == '__main__':
     main()
